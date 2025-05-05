2a488d1cbf1a40a19a1d3b94bbddd9bfa37cc59d
wahyudedik <wdedyk@gmail.com>
2025-05-05 10:44:32 +0700

    local storage dan bug operator

--- todo_challenge 2/README.md
+++ todo_challenge 2/README.md
@@ -11,3 +11,16 @@
 - Alice: Bug 1
 - Bob: Bug 2
 - Carol: Feature
+
+
+Kalian mendapatkan proyek To-Do List Web App dari client. Saat ini, aplikasi masih memiliki beberapa bug dan belum memiliki fitur filter task.
+Dalam waktu 1 jam, kerjakan bersama dalam tim:
+Perbaiki sebanyak mungkin bug yang ditemukan.
+
+Tambahkan fitur filter untuk hanya menampilkan task yang belum selesai.
+
+Tulis dokumentasi singkat di file README.md, berisi:
+Bug apa saja yang ditemukan & diperbaiki
+Deskripsi fitur yang ditambahkan
+Siapa mengerjakan apa
+https://drive.google.com/file/d/1K54nApM8bPRUOmzCDrFp9GbOvN3QfVLj/view?usp=share_link
--- todo_challenge 2/src/app.js
+++ todo_challenge 2/src/app.js
@@ -1,27 +1,34 @@
 const taskInput = document.getElementById("taskInput");
 const taskList = document.getElementById("taskList");
 let tasks = [];
+let currentFilter = 'all'; //default filter
+
+// load tasks from local storage
+document.addEventListener('DOMContentLoaded', loadTasksFromLocalStorage);
 
 function addTask() {
 	const taskText = taskInput.value;
 	if (taskText === "") return;
 
 	const task = {
+		id: Date.now(), //id unik untuk task
 		text: taskText,
 		completed: false,
-		createdAt: new Date(),
+		createdAt: new Date(), 
 	};
 
 	tasks.push(task);
+	saveTasksToLocalStorage(); //implementasi function saveTasksToLocalStorage
 	renderTasks();
+	taskInput.value = ""; // menambahkan input reset setelah masuk task
 }
 
 function renderTasks() {
 	taskList.innerHTML = "";
 	tasks.forEach((task, index) => {
 		const li = document.createElement("li");
-		li.textContent = task.text;
-		if ((task.completed = true)) {
+		li.textContent = task.text; 
+		if ((task.completed === true)) { //memperbaiki operator perbandingan = menjadi ===
 			li.style.textDecoration = "line-through";
 		}
 
@@ -33,4 +40,17 @@ function renderTasks() {
 function toggleTask(index) {
 	tasks[index].completed = !tasks[index].completed;
 	renderTasks();
+}
+
+function saveTasksToLocalStorage(){
+	localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+function loadTasksFromLocalStorage(){
+	const saveTasks = localStorage.getItem('tasks');
+	
+	if (saveTasks){
+		tasks = JSON.parse(saveTasks);
+		renderTasks();
+	}
 }
